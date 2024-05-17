4622db22b3e0c02f7b65f61aa6718a85569d4cb2
chonyy <mlchony7@gmail.com>
2024-05-17 14:47:47 -0400

    add bfs

--- test.py
+++ test.py
@@ -13,7 +13,34 @@ def parse_file(file_path):
     root_node = tree.children[0] 
     node_type = type(root_node).__name__
     
-    print(tree.toStringTree(recog=parser))
+    # print(tree.toStringTree(recog=parser))
+    bfs_tree(tree)
+
     pass
+
+from collections import deque
+
+def bfs_tree(tree):
+    """
+    Realiza una búsqueda en amplitud (BFS) sobre el árbol de análisis sintáctico.
+    
+    param tree: El árbol de análisis sintáctico generado por ANTLR.
+    """
+    
+    queue = deque([tree])
+    
+    while queue:
+        # Sacar el primer nodo de la cola
+        current_node = queue.popleft()
+        
+        # Imprimir el tipo de nodo
+        # print(type(current_node).__name__)
+        
+        try:
+            print(current_node.symbol.text)
+        except:
+            for child in current_node.children:
+                    queue.append(child)
+
 
 parse_file("CSharp/examples/archivo_csharp.cs")
